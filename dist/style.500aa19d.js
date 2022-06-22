ef6b48df74fc48fd6d5d81017a632551f7778dea
Gabriele Concas <gabrieleconcas96@gmail.com>
2022-06-22 19:44:03 +0200

    hamburger menu è un div

--- dist/style.500aa19d.js
+++ dist/style.500aa19d.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./..\\images\\icon-menu.svg":[["icon-menu.55d43bc7.svg","images/icon-menu.svg"],"images/icon-menu.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55167" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57589" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
